2c6bbd21176a71c6c75c30c0e609221113602fcf
bearmug <pavel.fadeev@gmail.com>
2016-08-15 00:00:52 +0300

    #12 resolve forall/exists problem

--- week-2/src/main/scala/funsets/FunSets.scala
+++ week-2/src/main/scala/funsets/FunSets.scala
@@ -62,23 +62,26 @@ object FunSets {
     */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > 1000) true
+      else if (s(a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-1000)
   }
 
   /**
     * Returns whether there exists a bounded integer within `s`
     * that satisfies `p`.
     */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    !forall(s, x => !p(x))
+  }
 
   /**
     * Returns a set transformed by applying `f` to each element of `s`.
     */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set =
+    (n: Int) => s(f(n))
 
   /**
     * Displays the contents of a set
