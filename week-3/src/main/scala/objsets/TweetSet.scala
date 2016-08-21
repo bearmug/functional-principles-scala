c055549c7fcd85b0756c0ea91504ff9805dcc8d0
bearmug <pavel.fadeev@gmail.com>
2016-08-21 22:55:36 +0300

    #20 commit target functional

--- week-3/src/main/scala/objsets/TweetSet.scala
+++ week-3/src/main/scala/objsets/TweetSet.scala
@@ -1,7 +1,5 @@
 package objsets
 
-import TweetReader._
-
 /**
  * A class to represent tweets.
  */
@@ -41,10 +39,10 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
-  
+  def filter(p: Tweet => Boolean): TweetSet
+
   /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
+   * This is a helper method for `filter` that propagates the accumulated tweets.
    */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
@@ -55,7 +53,7 @@ abstract class TweetSet {
    * and be implemented in the subclasses?
    */
     def union(that: TweetSet): TweetSet = ???
-  
+
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -66,7 +64,7 @@ abstract class TweetSet {
    * and be implemented in the subclasses?
    */
     def mostRetweeted: Tweet = ???
-  
+
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
    * in descending order. In other words, the head of the resulting list should
@@ -77,7 +75,7 @@ abstract class TweetSet {
    * and be implemented in the subclasses?
    */
     def descendingByRetweet: TweetList = ???
-  
+
   /**
    * The following methods are already implemented
    */
@@ -107,8 +105,8 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
   /**
    * The following methods are already implemented
    */
@@ -120,13 +118,24 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  /**
+    * This method takes a predicate and returns a subset of all the elements
+    * in the original set for which the predicate is true.
+    */
+  def filter(p: (Tweet) => Boolean): TweetSet = this
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+
+    def res = if (p(elem)) acc.incl(elem) else acc
+    right.filterAcc(p, left.filterAcc(p, res))
+  }
+
+
+
   /**
    * The following methods are already implemented
    */
@@ -152,6 +161,12 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     left.foreach(f)
     right.foreach(f)
   }
+
+  /**
+    * This method takes a predicate and returns a subset of all the elements
+    * in the original set for which the predicate is true.
+    */
+  def filter(p: (Tweet) => Boolean): TweetSet = filterAcc(p, new Empty)
 }
 
 trait TweetList {
@@ -182,7 +197,7 @@ object GoogleVsApple {
 
     lazy val googleTweets: TweetSet = ???
   lazy val appleTweets: TweetSet = ???
-  
+
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
