71cc3b465016af959fb96f4f228c1957d160cdb9
bearmug <pavel.fadeev@gmail.com>
2016-09-27 00:27:56 +0300

    #68 generate arbitrary heaps

--- course-2/week-3/src/main/scala/quickcheck/QuickCheck.scala
+++ course-2/week-3/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,7 +1,6 @@
 package quickcheck
 
 import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
@@ -9,10 +8,19 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    a <- Arbitrary.arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(a, h)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
