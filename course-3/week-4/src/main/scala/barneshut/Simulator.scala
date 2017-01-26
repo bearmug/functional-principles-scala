f362d6fb9b2dfa00717b39e7f981e6f199a8b4d5
bearmug <pavel.fadeev@gmail.com>
2017-01-26 21:09:05 +0300

    #88 implement combiners for sector matrix

--- course-3/week-4/src/main/scala/barneshut/Simulator.scala
+++ course-3/week-4/src/main/scala/barneshut/Simulator.scala
@@ -12,11 +12,21 @@ import common._
 class Simulator(val taskSupport: TaskSupport, val timeStats: TimeStatistics) {
 
   def updateBoundaries(boundaries: Boundaries, body: Body): Boundaries = {
-    ???
+    val res = new Boundaries()
+    res.minX = boundaries.minX min body.x
+    res.maxX = boundaries.maxX max body.x
+    res.minY = boundaries.minY min body.y
+    res.maxY = boundaries.maxY max body.y
+    res
   }
 
   def mergeBoundaries(a: Boundaries, b: Boundaries): Boundaries = {
-    ???
+    val res = new Boundaries()
+    res.minX = a.minX min b.minX
+    res.maxX = a.maxX max b.maxX
+    res.minY = a.minY min b.minY
+    res.maxY = a.maxY max b.maxX
+    res
   }
 
   def computeBoundaries(bodies: Seq[Body]): Boundaries = timeStats.timed("boundaries") {
@@ -28,7 +38,8 @@ class Simulator(val taskSupport: TaskSupport, val timeStats: TimeStatistics) {
   def computeSectorMatrix(bodies: Seq[Body], boundaries: Boundaries): SectorMatrix = timeStats.timed("matrix") {
     val parBodies = bodies.par
     parBodies.tasksupport = taskSupport
-    ???
+    parBodies.aggregate(
+      new SectorMatrix(boundaries, SECTOR_PRECISION))((sm, body) => sm += body, (sa, sb) => sa combine sb)
   }
 
   def computeQuad(sectorMatrix: SectorMatrix): Quad = timeStats.timed("quad") {
@@ -38,7 +49,7 @@ class Simulator(val taskSupport: TaskSupport, val timeStats: TimeStatistics) {
   def updateBodies(bodies: Seq[Body], quad: Quad): Seq[Body] = timeStats.timed("update") {
     val parBodies = bodies.par
     parBodies.tasksupport = taskSupport
-    ???
+    parBodies.map(_.updated(quad)).seq
   }
 
   def eliminateOutliers(bodies: Seq[Body], sectorMatrix: SectorMatrix, quad: Quad): Seq[Body] = timeStats.timed("eliminate") {
