1b55aa98fd540edc493bfd854827c126b654f808
bearmug <pavel.fadeev@gmail.com>
2017-04-09 21:58:47 +0300

    #96 solve data preparation problem

--- course-4/week-4/src/main/scala/timeusage/TimeUsage.scala
+++ course-4/week-4/src/main/scala/timeusage/TimeUsage.scala
@@ -1,9 +1,12 @@
 package timeusage
 
 import java.nio.file.Paths
+import java.util.regex.Pattern
 
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
+
+import scala.util.matching.Regex
 
 /** Main class */
 object TimeUsage {
@@ -88,9 +91,19 @@ object TimeUsage {
     * 3. other activities (leisure). These are the columns starting with “t02”, “t04”, “t06”, “t07”, “t08”, “t09”,
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
-  def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
-  }
+
+  val regexPrim = "(t01|t03|t11|t1801|t1803).*"
+  val regexWork = "(t05|t1805).*"
+  val regexLesr = "(t02|t04|t06|t07|t08|t09|t10|t12|t13|t14|t15|t16|t18).*"
+
+  def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = (
+    columnNames.filter(_.matches(regexPrim)).map(new Column(_)),
+    columnNames.filter(_.matches(regexWork)).map(new Column(_)),
+    columnNames.filter(s =>
+      !s.matches(regexPrim) &&
+        !s.matches(regexWork) &&
+        s.matches(regexLesr)).map(new Column(_))
+  )
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
     *         are summed together in a single column (and same for work and leisure). The “teage” column is also
--- course-4/week-4/src/test/scala/timeusage/TimeUsageSuite.scala
+++ course-4/week-4/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,19 +1,22 @@
 package timeusage
 
-import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
+import org.apache.spark.sql.Column
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-import scala.util.Random
-
 @RunWith(classOf[JUnitRunner])
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
 
+  test("classifiedColumns works just fine") {
+    val workCol = Set("t05", "t180544", "t053a")
+    val primCol = Set("t01", "t0345", "t11", "t1801aaa", "t1803")
+    val lesrCol = Set("t0200000", "t0444")
+    val alenCol = Set("alienCol")
+
+    val (p, w, l) = TimeUsage.classifiedColumns((Set.empty ++ workCol ++ primCol ++ lesrCol ++ alenCol).toList)
+    assert(p.toSet == primCol.map(new Column(_)))
+    assert(w.toSet == workCol.map(new Column(_)))
+    assert(l.toSet == lesrCol.map(new Column(_)))
+  }
 }
