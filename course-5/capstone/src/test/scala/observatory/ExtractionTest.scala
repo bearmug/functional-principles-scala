f878b2a6c4224eef49a7f354de2c4be6b2e6b154
bearmug <pavel.fadeev@gmail.com>
2017-04-16 21:29:12 +0300

    #98 common utility test submission

--- course-5/capstone/src/test/scala/observatory/ExtractionTest.scala
+++ course-5/capstone/src/test/scala/observatory/ExtractionTest.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,7 +9,14 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  ignore("locateTemperatures works fine for properly populated input") {
-
+  test("locateTemperatures works fine for properly populated input") {
+    assert(
+      Extraction.locateTemperatures(2015, "/stations-test.csv", "/2015-test.csv") ==
+        Seq(
+          (LocalDate.of(2015, 7, 11), Location(12.2, -20.01), 26.0),
+          (LocalDate.of(2015, 7, 12), Location(12.2, -20.01), 20.444444444444443),
+          (LocalDate.of(2015, 11, 13), Location(12.2, -20.01), 13.444444444444446)
+        )
+    )
   }
 }
