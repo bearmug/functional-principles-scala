b1b8d419e3c1207b33b44d1276706a66a1776cc9
bearmug <pavel.fadeev@gmail.com>
2017-04-16 19:28:06 +0300

    #98 solve basic import problem

--- course-5/capstone/src/main/scala/observatory/Extraction.scala
+++ course-5/capstone/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,14 @@
 package observatory
 
-import java.time.LocalDate
+import observatory.bearmug.ServiceLocator.Itr
+import observatory.bearmug.{ServiceLocator, TemperatureService}
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  lazy val service: TemperatureService = ServiceLocator.servePlain()
 
   /**
     * @param year             Year number
@@ -13,15 +16,15 @@ object Extraction {
     * @param temperaturesFile Path of the temperatures resource file to use (e.g. "/1975.csv")
     * @return A sequence containing triplets (date, location, temperature)
     */
-  def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+  def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Itr = {
+    service.temperaturesOf(year, stationsFile, temperaturesFile)
   }
 
   /**
     * @param records A sequence containing triplets (date, location, temperature)
     * @return A sequence containing, for each location, the average temperature over the year.
     */
-  def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
+  def locationYearlyAverageRecords(records: Itr): Iterable[(Location, Double)] = {
     ???
   }
 
---
+++ course-5/capstone/src/main/scala/observatory/bearmug/ServiceLocator.scala
@@ -0,0 +1,68 @@
+package observatory.bearmug
+
+import java.time.LocalDate
+
+import observatory.Location
+import observatory.bearmug.ServiceLocator.Itr
+
+import scala.io.{BufferedSource, Source}
+import scala.util.matching.Regex
+
+sealed abstract class TemperatureService {
+
+  def itr(year: Int, stnKey: String, stnSrc: BufferedSource, tmpSrc: BufferedSource): Itr
+
+  final def temperaturesOf(year: Int, stationsFile: String, temperaturesFile: String): Itr =
+    itr(
+      year,
+      stationsFile: String,
+      Source.fromInputStream(getClass.getResourceAsStream(stationsFile)),
+      Source.fromInputStream(getClass.getResourceAsStream(temperaturesFile))
+    )
+}
+
+object Conversions {
+  def toCelsius(fahrenheit: Double): Double = (fahrenheit - 32) * 5 / 9
+
+  def toFahrenheit(celsius: Double): Double = (celsius * 9 / 5) + 32
+}
+
+object ServiceLocator {
+
+  type Itr = Iterable[(LocalDate, Location, Double)]
+  val stnPattern: Regex = "(\\d+),(\\d+),(-?\\d+\\.\\d+),(-?\\d+\\.\\d+)".r
+  val tempPattern: Regex = "(\\d+),(\\d+),(\\d+),(\\d+),(\\d+\\.\\d+)".r
+
+  class PlainService extends TemperatureService {
+
+    def stationsData(stnSrc: BufferedSource): Map[String, Location] = stnSrc
+      .getLines()
+      .flatMap {
+        case stnPattern(stn, wban, latitude, longitude) =>
+          Option(s"$stn:$wban" -> Location(latitude.toDouble, longitude.toDouble))
+        case _ => None
+      }.toMap
+
+    override def itr(
+                      year: Int,
+                      stnKey: String,
+                      stnSrc: BufferedSource,
+                      tmpSrc: BufferedSource): Itr = {
+
+      val stationsMap = stationsData(stnSrc)
+
+      tmpSrc
+        .getLines()
+        .flatMap {
+          case tempPattern(stn, wban, month, day, tempF) => Some(
+            LocalDate.of(year, month.toInt, day.toInt),
+            stationsMap(s"$stn:$wban"),
+            Conversions.toCelsius(tempF.toDouble))
+          case _ => None
+        }.toIterable
+    }
+
+  }
+
+  def servePlain(): TemperatureService = new PlainService
+}
