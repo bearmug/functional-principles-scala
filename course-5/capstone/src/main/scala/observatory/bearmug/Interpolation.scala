3a466773fcc52fca423712e484b2fe939c260951
bearmug <pavel.fadeev@gmail.com>
2017-05-06 21:38:19 +0300

    #102 add great circle interpolation

--- course-5/capstone/src/main/scala/observatory/bearmug/Interpolation.scala
+++ course-5/capstone/src/main/scala/observatory/bearmug/Interpolation.scala
@@ -12,11 +12,18 @@ object Interpolation {
   private object PlainInterpolation extends Interpolation {
 
     val R = 6372.8
+    val P = 2
 
     def distance(loc1: Location, loc2: Location): Double =
       R * acos(sin(loc1.lat)*sin(loc2.lat)+cos(loc1.lat)*cos(loc2.lat)*cos(abs(loc1.lon - loc2.lon)))
 
-    def interpolate(temperatures: Iterable[(Location, Double)], location: Location): Double = ???
+    def interpolate(temperatures: Iterable[(Location, Double)], location: Location): Double =
+      temperatures.foldLeft((0.0, 0.0))((acc, loc) => {
+        val w = 1 / math.pow(distance(loc._1, location), P)
+        (acc._1 + w * loc._2, acc._2 + w)
+      }) match {
+        case (weightedSum, weights) => weightedSum / weights
+      }
 
     override def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
       temperatures.find(t => distance(t._1, location) < 1.0) match {
--- course-5/capstone/src/test/scala/observatory/bearmug/InterpolationTest.scala
+++ course-5/capstone/src/test/scala/observatory/bearmug/InterpolationTest.scala
@@ -1,13 +1,36 @@
 package observatory.bearmug
 
 import observatory.Location
+import observatory.bearmug.Interpolation.plain
 import org.scalatest.FunSuite
+
+import org.scalatest._
+import org.scalatest.Matchers._
 
 class InterpolationTest extends FunSuite {
 
   test("nearest point temperature chosen if distance less than 1.0 km") {
-    val interpolation: Interpolation = observatory.bearmug.Interpolation.plain
-    assert(interpolation.predictTemperature(List((Location(10, 10), 11)), Location(10.0001, 10.00001)) == 11)
+    assert(
+      plain.predictTemperature(List((Location(10, 10), 11)), Location(10.0001, 10.00001)) == 11
+    )
+  }
+
+  test("avg temperature chosen for two avg point") {
+    assert(
+      plain.predictTemperature(List((Location(10, 10), 0), (Location(12, 12), 20)), Location(11, 11)) === 10.0 +- .05
+    )
+  }
+
+  test("avg temperature chosen for four avg point") {
+    assert(
+      plain.predictTemperature(List(
+        (Location(10, 10), 0),
+        (Location(10, 12), 5),
+        (Location(12, 10), 15),
+        (Location(12, 12), 20)
+      ),
+        Location(11, 11)) === 10.0 +- .05
+    )
   }
 
 }
