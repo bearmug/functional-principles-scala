1064084bfe1eade7ca77b499d7f38a439e915bf3
bearmug <pavel.fadeev@gmail.com>
2017-05-18 23:36:40 +0300

    #106 calculate yearly avg temperatures

--- course-5/capstone/src/main/scala/observatory/Manipulation.scala
+++ course-5/capstone/src/main/scala/observatory/Manipulation.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import observatory.Visualization.predictTemperature
+
 /**
   * 4th milestone: value-added information
   */
@@ -10,9 +12,9 @@ object Manipulation {
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
-  def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double = {
-    ???
-  }
+  def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double =
+    (lat, lon) => predictTemperature(temperatures, Location(lat, lon))
+
 
   /**
     * @param temperaturess Sequence of known temperatures over the years (each element of the collection
@@ -20,12 +22,22 @@ object Manipulation {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Double)]]): (Int, Int) => Double = {
-    ???
+    (lat, lon) => {
+      val aggregate = temperaturess.aggregate(
+        (0.0, 0))(
+        (acc, yearData) => (acc, yearData) match {
+          case ((t, c), d) => (t + predictTemperature(d, Location(lat, lon)), c + 1)
+        },
+        (d1, d2) => (d1, d2) match {
+          case ((t1, c1), (t2, c2)) => (t1 + t2, c1 + c2)
+        })
+      aggregate._1 / aggregate._2
+    }
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param normals A grid containing the “normal” temperatures
+    * @param normals      A grid containing the “normal” temperatures
     * @return A sequence of grids containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double = {
